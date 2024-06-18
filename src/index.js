bc52cf017c02e823da5a4ab45ff2db6d586dae24
yogeshdnumb <dyogesh.official@gmail.com>
2024-06-18 19:28:42 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import "dotenv";
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: process.env.FRONTEND_URL },
+});
+
+io.use((socket, next) => {
+  socket.join(["general", "anime", "india"]);
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  socket.username = username;
+  next();
+});
+
+io.on("connection", (socket) => {
+  // console.log(`${socket.id.substring(0, 4)} connected`);
+
+  // to is roomId
+  socket.on("message", ({ text, from, to, isRoom }) => {
+    // console.log({ from, to, text, isRoom });
+    socket.to(to).emit("message", {
+      text,
+      username: socket.username,
+      from,
+      to,
+      isRoom,
+    });
+  });
+});
+
+httpServer.listen(process.env.PORT || 3000, () => {
+  console.log(`server running at http://localhost:${process.env.PORT || 3000}`);
+});
